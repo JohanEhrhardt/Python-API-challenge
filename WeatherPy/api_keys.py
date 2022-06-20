1da63f666313e907df998b9ed500a15365078969
JohanEhrhardt <johan.ehrhardt@hotmail.com>
2022-06-20 15:35:46 +1000

    c

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "4d1b5c3b75ab61f3bdeb5eeca5bfed27"
+weather_api_key = "Key Here"
 
 # Google API Key
-g_key = "AIzaSyCO54fKj9azSfWu0m1Om-Meur37Pn5jy2s"
+g_key = "Key Here"
